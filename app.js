20f6d00402f374fe645ba5c5e661bc1317a371eb
B Tarun kumar <tarunkumar.kgp2001@gmail.com>
2022-12-24 12:59:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.listen(3000, function () {
+  console.log("Server started at port 3000");
+});
