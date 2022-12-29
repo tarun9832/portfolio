9cf59ce55f1154b4b6891c60a35bec902399d75d
B Tarun kumar <tarunkumar.kgp2001@gmail.com>
2022-12-29 19:34:33 +0530

    made minor changes including shifting of script tag

--- public/js/index.js
+++ public/js/index.js
@@ -23,3 +23,23 @@ function openmenu(){
 function closemenu() {
   sidemeu.style.right = "-200px";
 }
+
+// for form content
+
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbyuMGjSbdmXTSFOnakDKdrPgP4ZxuJSyuuRPcuvHsyDzAHWLBFcY7wVbOTzi-oaWHRVWw/exec'
+        const form = document.forms['submit-to-google-sheet']
+        const msg = document.getElementById("msg")
+
+        form.addEventListener('submit', e => {
+            e.preventDefault()
+            fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+                .then(response => {msg.innerHTML = "Message sent successfully"
+            setTimeout(function(){
+                        msg.innerHTML = ""
+                    }, 5000)
+            form.reset()
+        })
+                
+                .catch(error => console.error('Error!', error.message))
+        })
+    
